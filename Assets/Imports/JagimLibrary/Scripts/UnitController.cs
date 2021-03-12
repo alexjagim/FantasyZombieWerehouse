ef79c91e2ccc39f9f3cb2d125b659c1c40521bd8
alexjagim <alexjagim@yahoo.com>
2021-03-11 22:44:25 -0600

    Added a Damaged animation to the characters

--- Assets/Imports/JagimLibrary/Scripts/UnitController.cs
+++ Assets/Imports/JagimLibrary/Scripts/UnitController.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using Sirenix.OdinInspector;
 using System;
+using System.Collections;
 
 public class UnitController : MonoBehaviour
 {
@@ -15,6 +16,19 @@ public class UnitController : MonoBehaviour
 
     [SerializeField, LabelText("Max: "), BoxGroup("Base Variables"), HorizontalGroup("Base Variables/Stamina"), LabelWidth(30)]
     private int _iMaxStamina;
+
+    private Material _material_Original;
+
+    [SerializeField, LabelText("Skinned Mesh Renderer"), BoxGroup("Base Variables"), Title("Damage Animation")]
+    private SkinnedMeshRenderer _skinnedMeshRenderer;
+
+    [SerializeField, LabelText("Damage Material"), BoxGroup("Base Variables")]
+    private Material _material_Damage;
+
+    [SerializeField, LabelText("Animation Delay"), BoxGroup("Base Variables")]
+    private float _fDamageAnimationDelay;
+    [SerializeField, LabelText("Animation Duration"), BoxGroup("Base Variables")]
+    private float _fDamageAnimationDuration;
 
     public event Action action_OnDestroy;
 
@@ -36,11 +50,21 @@ public class UnitController : MonoBehaviour
     protected virtual void InitVariables()
     {
         _iCurrentHealth = _iMaxHealth;
+        _material_Original = _skinnedMeshRenderer.material;
     }
 
     public void TakeDamage(int iDamage)
     {
         _iCurrentHealth -= iDamage;
+
+        //Clears out in the attack happens again before the damage flash is complete
+        StopCoroutine("FlashObject");
+        StartCoroutine(FlashObject(_skinnedMeshRenderer, _material_Original, _material_Damage, _fDamageAnimationDuration, _fDamageAnimationDelay));
+    }
+
+    protected virtual void DamageAnimation()
+    {
+        //FlashObject(_skinnedMeshRenderer, _material_Original, _material_Damage, 1.0f, 1.0f);
     }
 
     public int GetCurrentHealth()
@@ -67,5 +91,20 @@ public class UnitController : MonoBehaviour
     {
         action_OnDestroy?.Invoke();
         Destroy(gameObject);
+    }
+
+    IEnumerator FlashObject(SkinnedMeshRenderer toFlash, Material originalMaterial, Material flashMaterial, float flashTime, float flashDelay)
+    {
+        float flashingFor = 0;
+        Material newColor = flashMaterial;
+
+        yield return new WaitForSeconds(flashDelay);
+
+        toFlash.material = newColor;
+        flashingFor += Time.deltaTime;
+
+        yield return new WaitForSeconds(flashTime);
+
+        toFlash.material = originalMaterial;
     }
 }
